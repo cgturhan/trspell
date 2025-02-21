9bf3642aa7e3eadc0057934543acb19928c34aec
cgturhan <cerenguzell@gmail.com>
2025-02-22 00:54:18 +0100

    Update corrector_subwordbert.py

--- neuspell/corrector_subwordbert.py
+++ neuspell/corrector_subwordbert.py
@@ -1,3 +1,4 @@
+Dosya oluşturamaz, düzenleyemez veya yükleyemezsiniz … Yeterli depolama alanı yok.
 import os
 import time
 from typing import List, Dict, Union
@@ -5,13 +6,14 @@ from typing import List, Dict, Union
 import numpy as np
 import torch
 from pytorch_pretrained_bert import BertAdam
+import transformers
 
 from .commons import DEFAULT_TRAINTEST_DATA_PATH
 from .corrector import Corrector
-from .seq_modeling.helpers import bert_tokenize_for_valid_examples
+from .seq_modeling.helpers import bert_tokenize_for_valid_examples, roberta_tokenize_for_valid_examples, initialize_tokenizer
 from .seq_modeling.helpers import load_data, load_vocab_dict, save_vocab_dict
 from .seq_modeling.helpers import train_validation_split, batch_iter, labelize, progressBar, batch_accuracy_func
-from .seq_modeling.subwordbert import load_model, load_pretrained, model_predictions, model_inference
+from .seq_modeling.subwordbert import load_model, load_pretrained, load_pretrained_large, model_predictions, model_inference
 
 """ corrector module """
 
@@ -21,42 +23,87 @@ class BertChecker(Corrector):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.bert_pretrained_name_or_path = "bert-base-cased"
+        self.pretrained_name_or_path = "bert-base-cased"
+        self.tokenizer = transformers.BertTokenizer.from_pretrained("bert-base-cased")
 
     def load_model(self, ckpt_path):
         print(f"initializing model")
         initialized_model = load_model(self.vocab)
-        self.model = load_pretrained(initialized_model, self.ckpt_path, device=self.device)
+        self.model = load_pretrained(initialized_model, ckpt_path, device=self.device)
 
     def correct_strings(self, mystrings: List[str], return_all=False) -> List[str]:
         self.is_model_ready()
         mystrings = bert_tokenize_for_valid_examples(mystrings, mystrings, self.bert_pretrained_name_or_path)[0]
         data = [(line, line) for line in mystrings]
         batch_size = 4 if self.device == "cpu" else 16
-        return_strings = model_predictions(self.model, data, self.vocab, device=self.device, batch_size=batch_size)
+        return_strings = model_predictions(self.model, data, self.vocab, device=self.device, batch_size=batch_size, tokenizer = self.tokenizer)
         if return_all:
             return mystrings, return_strings
         else:
             return return_strings
 
-    def evaluate(self, clean_file, corrupt_file, data_dir=""):
+    def correct_from_file(self, src, dest="./clean_version.txt"):
+        """
+        src = f"{DEFAULT_DATA_PATH}/traintest/corrupt.txt"
+        """
+        self.__model_status()
+        x = [line.strip() for line in open(src, 'r')]
+        y = self.correct_strings(x)
+        print(f"saving results at: {dest}")
+        opfile = open(dest, 'w')
+        for line in y:
+            opfile.write(line + "\n")
+        opfile.close()
+        return
+    
+    def correct_data(self, clean_file, corrupt_file, data_dir=""):
         self.is_model_ready()
         data_dir = DEFAULT_TRAINTEST_DATA_PATH if data_dir == "default" else data_dir
+        batch_size = 4 if self.device == "cpu" else 16
+        test_data = load_data(data_dir, clean_file, corrupt_file)
 
+        preds = model_predictions(self.model,
+                                    test_data,
+                                    device=self.device,
+                                    batch_size=batch_size,
+                                    vocab=self.vocab,tokenizer = self.tokenizer)
+        return preds
+                
+
+    def evaluate(self, clean_file, corrupt_file, data_dir="", return_preds = False, limit_token_length = False):
+        self.is_model_ready()
+        data_dir = DEFAULT_TRAINTEST_DATA_PATH if data_dir == "default" else data_dir
+        model_name = self.pretrained_name_or_path 
+        preds = []
         batch_size = 4 if self.device == "cpu" else 16
         for x, y, z in zip([data_dir], [clean_file], [corrupt_file]):
             print(x, y, z)
             test_data = load_data(x, y, z)
-            _ = model_inference(self.model,
-                                test_data,
-                                topk=1,
-                                device=self.device,
-                                batch_size=batch_size,
-                                vocab_=self.vocab)
-        return
+            if return_preds:
+                preds = model_inference(self.model,
+                                    test_data,
+                                    topk=1,
+                                    device=self.device,
+                                    model_name = model_name,
+                                    batch_size=batch_size,
+                                    vocab_=self.vocab, limit_token_length = limit_token_length, tokenizer = self.tokenizer, return_preds= return_preds)
+                
+            else:
+                _ = model_inference(self.model,
+                                    test_data,
+                                    topk=1,
+                                    device=self.device,
+                                    model_name = model_name,
+                                    batch_size=batch_size,
+                                    vocab_=self.vocab,limit_token_length=limit_token_length, tokenizer = self.tokenizer)
+                
+        if return_preds:
+            return preds
+        else:
+            return
 
-    def from_huggingface(self, bert_pretrained_name_or_path, vocab: Union[Dict, str]):
-        self.bert_pretrained_name_or_path = bert_pretrained_name_or_path
+    def from_huggingface(self, pretrained_name_or_path, vocab: Union[Dict, str]):
+        self.pretrained_name_or_path = pretrained_name_or_path
         if isinstance(vocab, str) and os.path.exists(vocab):
             self.vocab_path = vocab
             print(f"loading vocab from path:{self.vocab_path}")
@@ -65,17 +112,33 @@ class BertChecker(Corrector):
             self.vocab = vocab
         else:
             raise ValueError(f"unknown vocab type or unable to find path: {type(vocab)}")
-        self.model = load_model(self.vocab, bert_pretrained_name_or_path=self.bert_pretrained_name_or_path)
-        self.model.to(self.device)
+        self.model = load_model(self.vocab, bert_pretrained_name_or_path=self.pretrained_name_or_path)
+        self.tokenizer = initialize_tokenizer(pretrained_name_or_path)
+        return
+
+    def from_pretrained(self, pretrained_name_or_path, ckpt_path, vocab: Union[Dict, str]):
+        self.pretrained_name_or_path = pretrained_name_or_path
+        if isinstance(vocab, str) and os.path.exists(vocab):
+            self.vocab_path = vocab
+            print(f"loading vocab from path:{self.vocab_path}")
+            self.vocab = load_vocab_dict(self.vocab_path)
+        elif isinstance(vocab, dict):
+            self.vocab = vocab
+        else:
+            raise ValueError(f"unknown vocab type or unable to find path: {type(vocab)}")
+        self.model = load_model(self.vocab, bert_pretrained_name_or_path=self.pretrained_name_or_path)
+        self.model.load_state_dict(torch.load(os.path.join(ckpt_path, 'pytorch_model.bin'), map_location=self.device))
+        self.tokenizer = initialize_tokenizer(pretrained_name_or_path)
         return
 
     def finetune(self,
                  clean_file,
                  corrupt_file,
                  data_dir="",
-                 ckpt_dir ="",
                  validation_split=0.2,
-                 n_epochs=2, 
+                 n_epochs=2,
+                 start_epoch = 0,
+                 model_name = 'bert-base',
                  batch_size = 16,
                  new_vocab_list: List = None):
 
@@ -85,7 +148,6 @@ class BertChecker(Corrector):
 
         # load data and split in train-validation
         data_dir = DEFAULT_TRAINTEST_DATA_PATH if data_dir == "default" else data_dir
-        ckpt_dir = self.DEFAULT_CHECKPOINT_PATH if ckpt_dir == "default" else ckpt_dir
         train_data = load_data(data_dir, clean_file, corrupt_file)
         
         val_clean_file = clean_file.replace("train", "val")
@@ -102,14 +164,14 @@ class BertChecker(Corrector):
         #############################################
         # training and validation
         #############################################
-        model, vocab = self.model, self.vocab
-        TRAIN_BATCH_SIZE, VALID_BATCH_SIZE = batch_size, batch_size #16, 32
+       
+        TRAIN_BATCH_SIZE, VALID_BATCH_SIZE = batch_size, batch_size
         GRADIENT_ACC = 4
         DEVICE = self.device
-        START_EPOCH, N_EPOCHS = 0, n_epochs
-        CHECKPOINT_PATH = os.path.join(ckpt_dir if ckpt_dir else data_dir, "new_models",
-                                       os.path.split(self.bert_pretrained_name_or_path)[-1])
-        if os.path.exists(CHECKPOINT_PATH):
+        START_EPOCH, N_EPOCHS = start_epoch, n_epochs
+        CHECKPOINT_PATH = os.path.join(self.ckpt_path if self.ckpt_path else data_dir, "new_models",
+                                       os.path.split(self.pretrained_name_or_path)[-1])
+        if os.path.exists(CHECKPOINT_PATH) and start_epoch==0:
             num = 1
             while True:
                 NEW_CHECKPOINT_PATH = CHECKPOINT_PATH + f"-{num}"
@@ -126,27 +188,19 @@ class BertChecker(Corrector):
         max_dev_acc, argmax_dev_acc = -1, -1
         patience = 100
 
-        # Create an optimizer
-        param_optimizer = list(model.named_parameters())
-        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-        optimizer_grouped_parameters = [
-            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
-            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-        ]
+
         t_total = int(len(train_data) / TRAIN_BATCH_SIZE / GRADIENT_ACC * N_EPOCHS)
         if t_total == 0:
             t_total = 1
-        optimizer = BertAdam(optimizer_grouped_parameters, lr=5e-5, warmup=0.1, t_total=t_total)
 
-        # model to device
-        model.to(DEVICE)
 
         # load parameters if not training from scratch
         if START_EPOCH > 1:
             progress_write_file = (
                 open(os.path.join(CHECKPOINT_PATH, f"progress_retrain_from_epoch{START_EPOCH}.txt"), 'w')
             )
-            model, optimizer, max_dev_acc, argmax_dev_acc = load_pretrained(model, CHECKPOINT_PATH, optimizer=optimizer)
+            self.model = load_pretrained(self.model, CHECKPOINT_PATH, optimizer=None)
+            #optimizer = BertAdam(optimizer_grouped_parameters, lr=5e-5, warmup=0.1, t_total=t_total)
             progress_write_file.write(f"Training model params after loading from path: {CHECKPOINT_PATH}\n")
         else:
             progress_write_file = open(os.path.join(CHECKPOINT_PATH, "progress.txt"), 'w')
@@ -154,8 +208,26 @@ class BertChecker(Corrector):
             progress_write_file.write(f"Training model params\n")
         progress_write_file.flush()
 
+        is_bert = True if 'bert-base' in model_name else False
+        # model to device
+        model, vocab = self.model, self.vocab
+
+        TOKENIZER = initialize_tokenizer(model_name)
+        self.tokenizer = TOKENIZER
+
+        # Create an optimizer
+        param_optimizer = list(model.named_parameters())
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        optimizer_grouped_parameters = [
+            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        ]
+        optimizer = BertAdam(optimizer_grouped_parameters, lr=5e-5, warmup=0.1, t_total=t_total)
+        
+        model.to(DEVICE)
+
         # train and eval
-        for epoch_id in range(START_EPOCH, N_EPOCHS + 1):
+        for epoch_id in range(START_EPOCH, START_EPOCH + N_EPOCHS + 1):
             # check for patience
             if (epoch_id - argmax_dev_acc) > patience:
                 print("patience count reached. early stopping initiated")
@@ -176,10 +248,15 @@ class BertChecker(Corrector):
             nbatches = int(np.ceil(len(train_data) / TRAIN_BATCH_SIZE))
             optimizer.zero_grad()
             for batch_id, (batch_labels, batch_sentences) in enumerate(train_data_iter):
+                
                 st_time = time.time()
                 # set batch data for bert
-                batch_labels_, batch_sentences_, batch_bert_inp, batch_bert_splits = \
-                    bert_tokenize_for_valid_examples(batch_labels, batch_sentences, self.bert_pretrained_name_or_path)
+                if is_bert:
+                    batch_labels_, batch_sentences_, batch_bert_inp, batch_bert_splits, _ = \
+                        bert_tokenize_for_valid_examples(batch_labels, batch_sentences, TOKENIZER)
+                else:
+                    batch_labels_, batch_sentences_, batch_bert_inp, batch_bert_splits, _ = \
+                        roberta_tokenize_for_valid_examples(batch_labels, batch_sentences, TOKENIZER)
                 if len(batch_labels_) == 0:
                     print("################")
                     print("Not training the following lines due to pre-processing mismatch: \n")
@@ -245,8 +322,13 @@ class BertChecker(Corrector):
             for batch_id, (batch_labels, batch_sentences) in enumerate(valid_data_iter):
                 st_time = time.time()
                 # set batch data for bert
-                batch_labels_, batch_sentences_, batch_bert_inp, batch_bert_splits = \
-                    bert_tokenize_for_valid_examples(batch_labels, batch_sentences, self.bert_pretrained_name_or_path)
+                if is_bert:
+                    batch_labels_, batch_sentences_, batch_bert_inp, batch_bert_splits, _ = \
+                        bert_tokenize_for_valid_examples(batch_labels, batch_sentences, TOKENIZER)
+                else:
+                    batch_labels_, batch_sentences_, batch_bert_inp, batch_bert_splits, _ = \
+                        roberta_tokenize_for_valid_examples(batch_labels, batch_sentences, TOKENIZER)
+
                 if len(batch_labels_) == 0:
                     print("################")
                     print("Not validating the following lines due to pre-processing mismatch: \n")
